fa67d7d0d64b8e224034fa6e669885a253c4505f
StefVM <91262422+StefVM@users.noreply.github.com>
2023-04-24 14:56:56 +0200

    Add files via upload
    Eventueel visueel board met SVGs

--- main.py
+++ main.py
@@ -1,39 +1,68 @@
 import chess
-import requests
+import chess.svg
+import urllib.parse
 
-board = chess.Board()
+
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(100, 100, 1100, 1100)
+
+        self.widgetSvg = QSvgWidget(parent=self)
+        self.widgetSvg.setGeometry(10, 10, 1080, 1080)
+
+        self.chessboard = chess.Board()
+
+        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
+        self.widgetSvg.load(self.chessboardSvg)
+
+    def paintEvent(self, event):
+        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
+        self.widgetSvg.load(self.chessboardSvg)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    board = MainWindow()
+    board.show()
+    app.exec()
+
+#board = chess.Board()
 
 def play_move(player_move):
     try:
-        move = board.parse_uci(player_move)
+        move = board.chessboard.parse_uci(player_move)
     except ValueError:
         print("Invalid move format, try again.")
         return
 
-    if move not in board.legal_moves:
+    if move not in board.chessboard.legal_moves:
         print("Illegal move, try again.")
         return
 
-    board.push(move)
+    board.chessboard.push(move)
+    MainWindow.paintEvent(board, move)
+    board.show()
+    app.exec()
     return get_board_image_url()
 
 
 def get_board_image_url():
-    fen = board.fen()
-    url = f"https://www.chess.com/dynboard?fen={fen}&size=3"
+    fen = board.chessboard.fen()
+    encoded_fen = urllib.parse.quote(fen, safe='')
+    base_url = "https://lichess.org/editor/"
+    url = base_url + encoded_fen
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.url
-    else:
-        print(f"Error getting board image URL: {response.status_code}")
-        return None
+    return url
 
 
-while not board.is_game_over():
+while not board.chessboard.is_game_over():
     player_move = input("move: ")
     image_url = play_move(player_move)
-    if image_url:
-        print(image_url)
+    print(image_url)
 
 print("Game over.")
