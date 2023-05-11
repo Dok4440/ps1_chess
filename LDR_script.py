75b1f1917cc0427aee260f74fff21f3f36d791d3
michaelverv <r0844989@student.thomasmore.be>
2023-05-11 08:56:29 +0200

    nieuwe code

--- LDR_script.py
+++ LDR_script.py
@@ -1,62 +1,87 @@
+import chess
+import requests
 import time
 import wiringpi
 import sys
-# import spidev
 
-# functions
+board = chess.Board()
 
-# ADC function
-# def readadc(adcnum):
-#     if ((adcnum > 7) or (adcnum < 0)):
-#         return -1
-#     revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1,(8+adcnum)<<4,0]))
-#     time.sleep(0.000005)
-#     adcout = ((recvData[1]&3) << 8) + recvData[2]
-#     return adcout
+def play_move(player_move):
+    try:
+        move = board.parse_uci(player_move)
+    except ValueError:
+        print("Invalid move format, try again.")
+        return
 
-# defines the LDR's
+    if move not in board.legal_moves:
+        print("Illegal move, try again.")
+        return
+
+    board.push(move)
+    return get_board_image_url()
+
+
+def get_board_image_url():
+    fen = board.fen()
+    url = f"https://www.chess.com/dynboard?fen={fen}&size=3"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.url
+    else:
+        print(f"Error getting board image URL: {response.status_code}")
+        return None
+
+def readadc(adcnum):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1,(8+adcnum)<<4,0]))
+    time.sleep(0.000005)
+    adcout = ((recvData[1]&3) << 8) + recvData[2]
+    return adcout
+
+def ActivateADC ():
+    wiringpi.digitalWrite(pin_CS_adc, 0) # Actived ADC using CS
+    time.sleep(0.000005)
+
+def DeactivateADC():
+    wiringpi.digitalWrite(pin_CS_adc, 1) # Deactived ADC using CS
+    time.sleep(0.000005)
+
 wiringpi.wiringPiSetup()
-LDR_E2 = wiringpi.pinMode(1,0)
-LDR_E4 = wiringpi.pinMode(3,0)
-LDR_G8 = wiringpi.pinMode(4,0)
-LDR_F6 = wiringpi.pinMode(6,0)
+pin_CS_adc = 16 #We will use w16 as CE, not the default pin w15!
+wiringpi.pinMode(pin_CS_adc, 1) # Set ce to mode 1 ( OUTPUT )
+wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0) #(channel, port, speed, mode)
 
-# Loop that checks for input/light
-try:
-    while True:
-        # Move of pawn
-        if LDR_E2 == 1:
-            print("pawn selected")
-            time.sleep(1)
+while not board.is_game_over():
 
-            if LDR_E4 == 1:
-               print("moving pawn to E4")
+    ActivateADC()
+    LDR_E2 = readadc(0)  # read from channel 0
+    DeactivateADC()
+    print(LDR_E2)
 
-            # aborts if nothing in selected in time
-            else:
-                print("no move selected in time, try again")
-            
-        # Move of knight
-        elif LDR_G8 == 1:
-            print("knight selected")
-            time.sleep(1)
+    if LDR_E2 < 70:
+        player_first_move = "e2"
+        print("pawn selected")
+        time.sleep(5)
 
-            if LDR_F6 == 1:
-               print("moving knight to F6")
+        ActivateADC()
+        LDR_E4 = readadc(1)  # read from channel 1
+        DeactivateADC()
+        print(LDR_E4)
 
-            # aborts if nothing in selected in time
-            else:
-                print("no move selected in time, try again")
+        if LDR_E4 < 70:
+            player_second_move = "e4"
+            print("moving pawn to", player_second_move)
 
-        # No move
+        # aborts if nothing in selected in time
         else:
-            print("no piece selected")
-            time.sleep(1)
+            print("no move selected in time, try again")
 
-        time.sleep(1) 
+    player_move = player_first_move +  player_second_move
+    image_url = play_move(player_move)
+    if image_url:
+        print(image_url)
 
-except KeyboardInterrupt:
-    print("Exit")
-    sys.exit(0)
 
-# als 1 en 0 niet werken --> ADC, waardes tussen 0 en 1023
+print("Game over.")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import chess
 import requests
+import time
+import wiringpi
+import sys
 
 board = chess.Board()
 
@@ -29,9 +32,50 @@ def get_board_image_url():
         print(f"Error getting board image URL: {response.status_code}")
         return None
 
+def readadc(adcnum):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1,(8+adcnum)<<4,0]))
+    time.sleep(0.000005)
+    adcout = ((recvData[1]&3) << 8) + recvData[2]
+    return adcout
+
+def ActivateADC ():
+    wiringpi.digitalWrite(pin_CS_adc, 0) # Actived ADC using CS
+    time.sleep(0.000005)
+
+def DeactivateADC():
+    wiringpi.digitalWrite(pin_CS_adc, 1) # Deactived ADC using CS
+    time.sleep(0.000005)
+
+wiringpi.wiringPiSetup()
+pin_CS_adc = 16 #We will use w16 as CE, not the default pin w15!
+wiringpi.pinMode(pin_CS_adc, 1) # Set ce to mode 1 ( OUTPUT )
+wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0) #(channel, port, speed, mode)
 
 while not board.is_game_over():
-    player_move = input("move: ")
+
+    ActivateADC()
+    LDR_E2 = readadc(0)  # read from channel 0
+    DeactivateADC()
+    ActivateADC()
+    LDR_E4 = readadc(1)  # read from channel 1
+    DeactivateADC()
+
+    if LDR_E2 < 70:
+        player_first_move = e2
+            print("pawn selected")
+            time.sleep(5)
+
+            if LDR_E4 < 70:
+                player_second_move = e4
+                print("moving pawn to", player_second_move)
+
+            # aborts if nothing in selected in time
+            else:
+                print("no move selected in time, try again")
+
+    player_move = player_first_move +  player_second_move
     image_url = play_move(player_move)
     if image_url:
         print(image_url)
